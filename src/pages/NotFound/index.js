a060c7b5c3899a69846c8402e954583b8bb6cc91
Bruno Henrique <brunohenriquex00@gmail.com>
2021-04-13 13:38:58 -0300

    pagina de erro

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection: 'column'}}>
+            <h1>Pagina de erro!</h1>
+            <h3>Endereço invalido</h3>
+        </div>
+    );
+}
